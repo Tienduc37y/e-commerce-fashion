67b4a21ec04f1af350298f5e359f13e11963c0aa
Tienduc37y <laitienduc37@gmail.com>
2024-08-08 09:46:44 +0700

    setting base project

---
+++ frontend/e-commerce/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Navigation from './customer/components/Navigation/Navigation'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Navigation/>
+      <div>home</div>
+    </div>
+  )
+}
+
+export default App
